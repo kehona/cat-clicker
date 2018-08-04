48ecf6c52b0a13156ba562ea15742e5dac55a383
Kehinde Onadipe <kensam@Kehindes-MacBook-Pro.local>
2018-08-03 22:47:59 -0500

    remodel cat clicker

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,90 +5,116 @@
  * handles click event on image
  */
 (function() {
-    let cats = [];
-    function Cat(name, image) {
-        this.name = name;
-        this.imagePath = image;
-        this.points = 0;
-        this.show = false;
+
+    let model = {
+        cats: [{
+                'name': 'cat1',
+                'imagePath': 'resources/img/cat_big.jpg',
+                'points': 0,
+            },
+            {
+                'name': 'cat2',
+                'imagePath': 'resources/img/cat2.jpg',
+                'points': 0,
+            },
+            {
+                'name': 'cat3',
+                'imagePath': 'resources/img/cat3.jpg',
+                'points': 0,
+            },
+            {
+                'name': 'cat4',
+                'imagePath': 'resources/img/cat4.jpg',
+                'points': 0,
+            },
+            {
+                'name': 'cat5',
+                'imagePath': 'resources/img/cat5.jpg',
+                'points': 0,
+            },
+        ],
+        selectedCat: null
     }
 
-    function createCats() {
-        const cat1 = new Cat('cat1', 'resources/img/cat_big.jpg');
-        cat1.show = true;
-        cats.push(cat1);
-        const cat2 = new Cat('cat2', 'resources/img/cat2.jpg');
-        cats.push(cat2);
-        const cat3 = new Cat('cat3', 'resources/img/cat3.jpg');
-        cats.push(cat3);
-        const cat4 = new Cat('cat4', 'resources/img/cat4.jpg');
-        cats.push(cat4);
-        const cat5 = new Cat('cat5', 'resources/img/cat5.jpg');
-        cats.push(cat5);
-    }
-
-    function createDomElements() {
-        for (const [index, cat] of cats.entries()) {    
-            console.log('Hello')
-            if (cat.show === true) {
-                let catDom = `<div id="info">
-                                <h3 id="cat-name">${cat.name}</h3>
-                                <h3 id="cat-click">Clicks: <span id="points-${cat.name}">0</span></h1>
-                            </div>
-                            <div id="cat-image">
-                                <img src=${cat.imagePath } id=${cat.name} alt="">
-                            </div>`
-                const parent = document.getElementById('cat');
-                parent.insertAdjacentHTML('afterbegin', catDom);
-            } 
-            
-            
+    let octopus = {
+        getSelectedCat: function() {
+            return model.selectedCat;
+        },
+        updateSelectCat: function(cat) {
+           model.selectedCat = cat;
+        },
+        getAllCats: function() {
+            return model.cats;
+        },
+        init: function() {
+            model.selectedCat = model.cats[0];
+            viewList.init();
+            viewImage.init();
         }
-        
     }
 
-    function createCatNamesDOM() {
-        const ul = document.getElementById('cat-list');
-        cats.forEach((cat) => {
-            let catName = `<li id=${cat.name} class='name'>${cat.name}</li>`;
-            ul.insertAdjacentHTML('beforeend', catName);
-        });
-       
+
+    let viewList = {
+        init: function() {
+            // load the cat names
+            this.catName = document.getElementById('cat-name');
+            this.catPoints = document.getElementById('cat-points');
+            this.catImage = document.getElementById('cat-image');
+
+            const cats = octopus.getAllCats();
+            const ul = document.getElementById('cat-list');
+            //
+            cats.forEach((cat) => {
+                let li = document.createElement('li');
+                li.id = cat.name;
+                li.className = 'name';
+                li.innerText = cat.name;
+                li.addEventListener('click', function(){
+                    let id = this.getAttribute('id');  // get the id of the selected cat
+                    let index = Number.parseInt(id.slice(-1)); // get the last digit in the id and convert to number
+                    let selectedCat = cats[index - 1];
+                    // update the selected cat
+                    octopus.updateSelectCat(selectedCat);
+                    viewList.render();
+                    // set current item and render.
+                });
+                ul.appendChild(li);
+            });
+            viewList.render(); 
+        },
+        render: function() {
+            let selectedCat = octopus.getSelectedCat();
+            this.catName.innerHTML = selectedCat.name;
+            this.catPoints.innerHTML = selectedCat.points;
+            this.catImage.innerHTML = `<img src=${selectedCat.imagePath } id=${selectedCat.name} alt="">`
+        }
+    }  
+
+    let viewImage = {
+        init: function() {
+            this.catName = document.getElementById('cat-name');
+            this.catPoints = document.getElementById('cat-points');
+            this.catImage = document.getElementById('cat-image');
+            
+            const imageDiv = document.getElementById('cat-image');
+            imageDiv.addEventListener('click', () => {
+                let selected = octopus.getSelectedCat();
+                selected.points += 1;
+
+                viewImage.render();
+            });
+        },
+        render: function() {
+            let selectedCat = octopus.getSelectedCat();
+            console.log(selectedCat)
+            this.catName.innerHTML = selectedCat.name;
+            this.catPoints.innerHTML = selectedCat.points;
+            this.catImage.innerHTML = `<img src=${selectedCat.imagePath } id=${selectedCat.name} alt="">`
+        }
     }
 
-    function init() {
-        createCats();
-        createCatNamesDOM();
-        createDomElements();
-    }
-    init();
+    octopus.init();
 
-    // let counter = 0;
-    // const imagesDiv = document.querySelector('section');
-    // console.log(imagesDiv)
-    // let pointSpan = document.querySelector('#total-points');
-    // imagesDiv.addEventListener('click', (e) => {
-    //     console.log(e.target)
-    //     counter++;
-    //     pointSpan.innerHTML = counter;
-    // });
-    
-    // const images = document.getElementsByClassName('image');
-    // for (let i = 0; i < images.length; i++) {
-    //     let count = 0;
-    //     console.log(i);
-    //     console.log(images[i])
-    //     images[i].addEventListener('click', (function(clickCount) {
-    //         let imageCount = clickCount;
-    //         return function() {
-    //             imageCount += 1;
-    //             let catClicks = document.querySelector(`#points-cat${i + 1}`);
-    //             // update image count
-    //             catClicks.innerHTML = imageCount;
-    //             return imageCount;
-    //         }
-    //     })(count));
-    // }
 }());
 
 
